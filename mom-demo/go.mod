425bbf38af1aa23c45806ea645e1617b7b280837
Sergey <serega.niolaev@yandex.ru>
2025-08-14 22:43:36 +0300

    first program

--- mom-demo/go.mod
+++ mom-demo/go.mod
@@ -1,1 +1,8 @@
 module mom-demo
+
+go 1.25.0
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/redis/go-redis v6.15.9+incompatible // indirect
+)
--- mom-demo/main.go
+++ mom-demo/main.go
@@ -1,13 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
-// возвращает сумму двух целых чисел a и b
-func sum(a, b int) int {
-	return a + b
-}
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	result := sum(3, 5)           // вызываем функцию sum() и сохраняем в переменную result
-	fmt.Println("Сумма:", result) // выводим на экран
+  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+  s := "gopher"
+  fmt.Printf("Hello and welcome, %s!\n", s)
+
+  for i := 1; i <= 5; i++ {
+	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
+	fmt.Println("i =", 100/i)
+  }
 }
