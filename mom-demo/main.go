8dafc5da57c116bf74276811f1eb0a9911aeec0d
Sergey <serega.niolaev@yandex.ru>
2025-08-20 13:10:29 +0300

    example of built-in functions

--- mom-demo/main.go
+++ mom-demo/main.go
@@ -2,12 +2,54 @@ package main
 
 import "fmt"
 
-// возвращает сумму двух целых чисел a и b
-func sum(a, b int) int {
-	return a + b
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, -15, 0, -16}
+	mappedArray := filterNumbers(arr, func(x int) bool {
+		return x > 0
+	})
+	fmt.Println(mappedArray)
+	fmt.Println(arr)
+	fmt.Println("Задача 1 ", mappedArray)
+
+	arrTemperatures := []float64{0, 20, 100, -10}
+	changeTemp := convertTemperatures(arrTemperatures, func(x float64) float64 {
+		return float64((x * 9 / 5) + 32)
+	})
+	fmt.Println("Задача 2 ", changeTemp)
+
+	arrString := []string{"hello", "world", "go", "programming"}
+	arrLength := stringLengths(arrString, func(s string) int {
+		return len(s)
+	})
+	fmt.Println("Задача 3 ", arrLength)
 }
 
-func main() {
-	result := sum(3, 5)           // вызываем функцию sum() и сохраняем в переменную result
-	fmt.Println("Сумма:", result) // выводим на экран
+type filterFunc func(x int) bool
+type convertFunc func(x float64) float64
+
+type lengthFunc func(string) int
+
+func filterNumbers(arr []int, f filterFunc) []int {
+	newarr := []int{}
+	for i := 0; i < len(arr); i++ {
+		if f(arr[i]) {
+			newarr = append(newarr, arr[i])
+		}
+	}
+	return newarr
+}
+
+func convertTemperatures(arr []float64, f convertFunc) []float64 {
+	for i := 0; i < len(arr); i++ {
+		arr[i] = f(arr[i])
+	}
+	return arr
+}
+
+func stringLengths(arr []string, f lengthFunc) []int {
+	newarr := []int{}
+	for i := 0; i < len(arr); i++ {
+		newarr = append(newarr, f(arr[i]))
+	}
+	return newarr
 }
